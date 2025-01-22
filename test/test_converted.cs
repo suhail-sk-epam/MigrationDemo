35dd1647cfc22e13786282ae3512515f076fcd62
suhail-sk-epam <102290328+suhail-sk-epam@users.noreply.github.com>
2025-01-22 18:14:07 +0530

    Create test/test_converted.cs

---
+++ test/test_converted.cs
@@ -0,0 +1,64 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+namespace PlaywrightSpecFlowTests
+{
+    [Binding]
+    public class TestSteps
+    {
+        private IPage _page;
+        private IBrowser _browser;
+        private IBrowserContext _context;
+
+        [BeforeScenario]
+        public async Task Setup()
+        {
+            var playwright = await Playwright.CreateAsync();
+            _browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+            _context = await _browser.NewContextAsync();
+            _page = await _context.NewPageAsync();
+            await _page.GotoAsync("https://devexpress.github.io/testcafe/example/");
+        }
+
+        [AfterScenario]
+        public async Task Teardown()
+        {
+            await _context.CloseAsync();
+            await _browser.CloseAsync();
+        }
+
+        [Given("I have navigated to the test page")]
+        public void GivenIHaveNavigatedToTheTestPage()
+        {
+            // Navigation is handled in Setup
+        }
+
+        [When("I type the name 'Peter' into the name input")]
+        public async Task WhenITypeTheNamePeterIntoTheNameInput()
+        {
+            await _page.FillAsync("#developer-name", "Peter");
+        }
+
+        [When("I replace it with 'Parker'")]
+        public async Task WhenIReplaceItWithParker()
+        {
+            await _page.FillAsync("#developer-name", "Parker");
+        }
+
+        [When("I correct it to 'Parker' with a caret position of 2")]
+        public async Task WhenICorrectItToParkerWithACaretPositionOf2()
+        {
+            await _page.FillAsync("#developer-name", "Parker");
+        }
+
+        [Then("the name input should contain 'Parker'")]
+        public async Task ThenTheNameInputShouldContainParker()
+        {
+            var value = await _page.InputValueAsync("#developer-name");
+            value.Should().Be("Parker");
+        }
+    }
+}
