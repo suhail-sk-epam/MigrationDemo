29eecaa0dae7bd988a371efd0afb431f7f5b3c7e
suhail-sk-epam <102290328+suhail-sk-epam@users.noreply.github.com>
2025-01-22 18:45:49 +0530

    Create test/test_converted.cs

---
+++ test/test_converted.cs
@@ -0,0 +1,54 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [Binding]
+    public class TestSteps
+    {
+        private IPage _page;
+        private IBrowser _browser;
+        private IBrowserContext _context;
+
+        [BeforeScenario]
+        public async Task Setup()
+        {
+            var playwright = await Playwright.CreateAsync();
+            _browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+            _context = await _browser.NewContextAsync();
+            _page = await _context.NewPageAsync();
+            await _page.GotoAsync("https://devexpress.github.io/testcafe/example/");
+        }
+
+        [AfterScenario]
+        public async Task Teardown()
+        {
+            await _context.CloseAsync();
+            await _browser.CloseAsync();
+        }
+
+        [Given(@"I type the name 'Peter'")]
+        public async Task GivenITypeTheNamePeter()
+        {
+            await _page.FillAsync("#developer-name", "Peter");
+        }
+
+        [When(@"I replace it with 'Parker'")]
+        public async Task WhenIReplaceItWithParker()
+        {
+            await _page.FillAsync("#developer-name", "Parker");
+        }
+
+        [Then(@"the name should be 'Parker'")]
+        public async Task ThenTheNameShouldBeParker()
+        {
+            var value = await _page.InputValueAsync("#developer-name");
+            value.Should().Be("Parker");
+        }
+
+        // Additional tests can be added here following the same pattern
+    }
+}
