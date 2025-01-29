75bb3da9ea3a5db23ca3b74fbb1e83193b99a5b5
suhail-sk-epam <102290328+suhail-sk-epam@users.noreply.github.com>
2025-01-29 15:52:15 +0530

    Update test/test_converted.cs

--- test/test_converted.cs
+++ test/test_converted.cs
@@ -1,8 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FluentAssertions;
-using TechTalk.SpecFlow;
+using SpecFlow;
 using Microsoft.Playwright;
-using System.Threading.Tasks;
 
 namespace TestProject
 {
@@ -30,25 +29,99 @@ namespace TestProject
             await _browser.CloseAsync();
         }
 
-        [Given(@"I type the name 'Peter'")]
-        public async Task GivenITypeTheNamePeter()
+        [Given(@"I type text in the input field")]
+        public async Task GivenITypeTextInTheInputField()
         {
             await _page.FillAsync("#developer-name", "Peter");
-        }
-
-        [When(@"I replace it with 'Parker'")]
-        public async Task WhenIReplaceItWithParker()
-        {
             await _page.FillAsync("#developer-name", "Parker");
-        }
-
-        [Then(@"the name should be 'Parker'")]
-        public async Task ThenTheNameShouldBeParker()
-        {
             var value = await _page.InputValueAsync("#developer-name");
             value.Should().Be("Parker");
         }
 
-        // Additional tests can be added here following the same pattern
+        [Given(@"I click an array of labels and check their states")]
+        public async Task GivenIClickAnArrayOfLabelsAndCheckTheirStates()
+        {
+            var labels = await _page.QuerySelectorAllAsync(".feature label");
+            foreach (var label in labels)
+            {
+                await label.ClickAsync();
+                var checkbox = await label.QuerySelectorAsync("input[type=checkbox]");
+                var isChecked = await checkbox.IsCheckedAsync();
+                isChecked.Should().BeTrue();
+            }
+        }
+
+        [Given(@"I deal with text using keyboard")]
+        public async Task GivenIDealWithTextUsingKeyboard()
+        {
+            await _page.FillAsync("#developer-name", "Peter Parker");
+            await _page.ClickAsync("#developer-name", new ClickOptions { Position = new Position { X = 5, Y = 5 } });
+            await _page.PressAsync("#developer-name", "Backspace");
+            var value = await _page.InputValueAsync("#developer-name");
+            value.Should().Be("Pete Parker");
+            await _page.PressAsync("#developer-name", "Home Right . Delete Delete Delete");
+            value = await _page.InputValueAsync("#developer-name");
+            value.Should().Be("P. Parker");
+        }
+
+        [Given(@"I move the slider")]
+        public async Task GivenIMoveTheSlider()
+        {
+            var initialOffset = await _page.EvalOnSelectorAsync<int>("#slider", "el => el.offsetLeft");
+            await _page.ClickAsync("#tried-test-cafe");
+            await _page.DragAndDropAsync("#slider", "#slider-tick-9");
+            var newOffset = await _page.EvalOnSelectorAsync<int>("#slider", "el => el.offsetLeft");
+            newOffset.Should().BeGreaterThan(initialOffset);
+        }
+
+        [Given(@"I deal with text using selection")]
+        public async Task GivenIDealWithTextUsingSelection()
+        {
+            await _page.FillAsync("#developer-name", "Test Cafe");
+            await _page.SelectTextAsync("#developer-name", 1, 7);
+            await _page.PressAsync("#developer-name", "Delete");
+            var value = await _page.InputValueAsync("#developer-name");
+            value.Should().Be("Tfe");
+        }
+
+        [Given(@"I handle native confirmation dialog")]
+        public async Task GivenIHandleNativeConfirmationDialog()
+        {
+            await _page.ClickAsync("#populate");
+            var dialog = await _page.WaitForEventAsync(PageEvent.Dialog);
+            await dialog.AcceptAsync();
+            var dialogHistory = await _page.EvalOnSelectorAllAsync<string>("#dialog-history", "els => els.map(el => el.innerText)");
+            dialogHistory[0].Should().Be("Reset information before proceeding?");
+            await _page.ClickAsync("#submit-button");
+            var result = await _page.InnerTextAsync("#result");
+            result.Should().Contain("Peter Parker");
+        }
+
+        [Given(@"I pick option from select")]
+        public async Task GivenIPickOptionFromSelect()
+        {
+            await _page.SelectOptionAsync("#preferred-interface", "Both");
+            var value = await _page.InputValueAsync("#preferred-interface");
+            value.Should().Be("Both");
+        }
+
+        [Given(@"I fill a form")]
+        public async Task GivenIFillAForm()
+        {
+            await _page.FillAsync("#developer-name", "Bruce Wayne");
+            await _page.ClickAsync("#macos");
+            await _page.ClickAsync("#tried-test-cafe");
+            await _page.FillAsync("#comments", "It's...");
+            await _page.WaitForTimeoutAsync(500);
+            await _page.FillAsync("#comments", "\ngood");
+            await _page.WaitForTimeoutAsync(500);
+            await _page.SelectTextAreaContentAsync("#comments", 1, 0);
+            await _page.PressAsync("#comments", "Delete");
+            await _page.FillAsync("#comments", "awesome!!!");
+            await _page.WaitForTimeoutAsync(500);
+            await _page.ClickAsync("#submit-button");
+            var result = await _page.InnerTextAsync("#result");
+            result.Should().Contain("Bruce Wayne");
+        }
     }
 }
